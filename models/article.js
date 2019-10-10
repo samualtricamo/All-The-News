688487047dc68783a662fb5e761dec7f37f611a4
Sam Tricamo <samualtricamo@gmail.com>
2019-10-10 15:51:16 -0500

    index added

--- models/article.js
+++ models/article.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
-var schema = mongoose.schema;
+var Schema = mongoose.Schema;
 
-var articleschema = new schema({
+var ArticleSchema = new Schema({
     title: {
         type: String,
         required: true,
@@ -10,23 +10,7 @@ var articleschema = new schema({
         type: String,
         required: true,
     },
-    summary: {
-        type: String,
-        default: "Summary Not Available",
-    },
-    issaved: {
-        type: Boolean,
-        default: false
-    },
-    status: {
-        type: String,
-        default: "Save Article"
-    },
-    created: {
-        type: Date,
-        default: Date.now
-    },
-    node: {
+    note: {
         type: schema.Types.ObjectId,
         ref: "Note"
     }
@@ -34,5 +18,5 @@ var articleschema = new schema({
 
 articleschema.index({title: "text"});
 
-var Article = mongoose.model("Article", articleschema);
+var Article = mongoose.model("Article", Articleschema);
 module.exports = Article;
