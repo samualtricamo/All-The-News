828c52741d714de098ea2daad4045ffa4dc14dfd
Sam Tricamo <samualtricamo@gmail.com>
2019-10-10 16:07:08 -0500

    changing public folder

--- models/article.js
+++ models/article.js
@@ -1,22 +1,34 @@
 var mongoose = require("mongoose");
+
 var Schema = mongoose.Schema;
 
 var ArticleSchema = new Schema({
-    title: {
-        type: String,
-        required: true,
-    },
-    link: {
-        type: String,
-        required: true,
-    },
-    note: {
-        type: schema.Types.ObjectId,
-        ref: "Note"
-    }
+
+  headline: {
+    type: String,
+    unique: true
+  },
+  summary: {
+    type: String
+    
+  },
+  url: {
+    type: String
+  },
+
+  isSaved: {
+    type: Boolean,
+    default: false
+  },
+
+ 
+  note: {
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }
 });
 
-articleschema.index({title: "text"});
+var Article = mongoose.model("Article", ArticleSchema);
 
-var Article = mongoose.model("Article", Articleschema);
+
 module.exports = Article;
