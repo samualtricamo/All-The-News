b60f742e7ea4420837ab58f595aae344b1e3850b
Sam Tricamo <samualtricamo@gmail.com>
2019-10-10 16:28:43 -0500

    server.js under construction

--- server.js
+++ server.js
@@ -1,161 +1,92 @@
 var express = require("express");
-var method = require("method-override");
-var body = require("body-parser");
-var exphbs = require("express-handlebars");
 var mongoose = require("mongoose");
 var logger = require("morgan");
 var cheerio = require("cheerio");
 var request = require("request");
 
-var note = require("./models/note");
-var article = require("./models/article")
-var databaseUrl = "mongodb://localhost/scrap";
+var db = require("./models");
 
-
-if (process.env.MONGODB_URI) {
-	mongoose.connect(process.env.MONGODB_URI);
-}
-else {
-	mongoose.connect(databaseUrl);
-};
-
-mongoose.Promise = Promise;
-var db = mongoose.connection;
-
-db.on("error", function(error) {
-	console.log("Mongoose Error: ", error);
-});
-
-db.once("open", function() {
-	console.log("Mongoose connection successful.");
-});
+const PORT = process.env.PORT || 2000;
 
 var app = express();
-var port = process.env.PORT || 3000;
 
-// app set-ups
+app.use(express.urlencoded({
+	extended: true
+  }));
+  app.use(express.json());
 
-app.use(logger("dev"));
-app.use(express.static("public"));
-app.use(body.urlencoded({extended: false}));
-app.use(method("_method"));
-app.engine("handlebars", exphbs({defaultLayout: "main"}));
-app.set("view engine", "handlebars");
+  app.use(express.static("public"));
 
-app.listen(port, function() {
-	console.log("Listening on port " + port);
-})
+  var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
+mongoose.connect(MONGODB_URI);
 
-// routes
+app.get('/scrape', function (req, res) {
+  
+	axios.get("https://stackoverflow.com/").then(function (response) {
+  
+		var $ = cheerio.load(response.data);
+		var result = {};
 
-app.get("/", function(req, res) {
-    article.find({}, null, {sort: {created: -1}}, function(err, data) {
-		if(data.length === 0) {
-			res.render("placeholder", {message: "There's nothing scraped yet. Please click \"Scrape For Newest Articles\" for fresh and delicious news."});
-		}
-		else{
-			res.render("index", {articles: data});
-		}
+		$("a.question-hyperlink").each(function (i, element) {
+
+			result.title = $(this).text();
+			result.link = $(this).attr("href");
+	  
+			db.Question.create(result)
+        .then(function (dbQuestion) {
+			console.log(dbQuestion);
+        })
+        .catch(function (err) {
+          return res.json(err);
+		});
+		res.send("Hey!!! You were able to sucessfully scrape!!!");
+    });
+  });
+});
+
+app.get("/Questions", function (req, res) {
+	db.Question.find({})
+	  .then(function (dbQuestion) {
+		res.json(dbQuestion);
+	  })
+	  .catch(function (err) {
+		res.json(err);
+	  });
+  });
+  
+  app.get("/Questions/:id", function (req, res) {
+	db.Question.findOne({
+		_id: req.params.id
+	  })
+	  .populate("note")
+	  .then(function (dbQuestion) {
+		res.json(dbQuestion);
+    })
+    .catch(function (err) {
+      res.json(err);
     });
 });
 
-
-app.get("/scrape", function(req, res) {
-	request("https://www.nytimes.com/section/world", function(error, response, html) {
-		var $ = cheerio.load(html);
-		var result = {};
-		$("div.story-body").each(function(i, element) {
-			var link = $(element).find("a").attr("href");
-			var title = $(element).find("h2.headline").text().trim();
-			var summary = $(element).find("p.summary").text().trim();
-			var img = $(element).parent().find("figure.media").find("img").attr("src");
-			result.link = link;
-			result.title = title;
-			if (summary) {
-				result.summary = summary;
-			};
-			if (img) {
-				result.img = img;
-			}
-			else {
-				result.img = $(element).find(".wide-thumb").find("img").attr("src");
-			};
-			var entry = new Article(result);
-			Article.find({title: result.title}, function(err, data) {
-				if (data.length === 0) {
-					entry.save(function(err, data) {
-						if (err) throw err;
-					});
-				}
-			});
-		});
-		console.log("Scrape finished.");
-		res.redirect("/");
-	});
-});
-
-app.get("/saved", function(req, res) {
-	Article.find({issaved: true}, null, {sort: {created: -1}}, function(err, data) {
-		if(data.length === 0) {
-			res.render("placeholder", {message: "You have not saved any articles yet. Try to save some delicious news by simply clicking \"Save Article\"!"});
-		}
-		else {
-			res.render("saved", {saved: data});
-		}
-	});
-});
-
-app.get("/:id", function(req, res) {
-	Article.findById(req.params.id, function(err, data) {
-		res.json(data);
-	})
-})
-
-app.post("/search", function(req, res) {
-	console.log(req.body.search);
-	Article.find({$text: {$search: req.body.search, $caseSensitive: false}}, null, {sort: {created: -1}}, function(err, data) {
-		console.log(data);
-		if (data.length === 0) {
-			res.render("placeholder", {message: "Nothing has been found. Please try other keywords."});
-		}
-		else {
-			res.render("search", {search: data})
-		}
-	})
-});
-
-app.post("/save/:id", function(req, res) {
-	Article.findById(req.params.id, function(err, data) {
-		if (data.issaved) {
-			Article.findByIdAndUpdate(req.params.id, {$set: {issaved: false, status: "Save Article"}}, {new: true}, function(err, data) {
-				res.redirect("/");
-			});
-		}
-		else {
-			Article.findByIdAndUpdate(req.params.id, {$set: {issaved: true, status: "Saved"}}, {new: true}, function(err, data) {
-				res.redirect("/saved");
-			});
-		}
-	});
-});
-
-app.post("/note/:id", function(req, res) {
-	var note = new Note(req.body);
-	note.save(function(err, doc) {
-		if (err) throw err;
-		Article.findByIdAndUpdate(req.params.id, {$set: {"note": doc._id}}, {new: true}, function(err, newdoc) {
-			if (err) throw err;
-			else {
-				res.send(newdoc);
-			}
+app.post("/Questions/:id", function (req, res) {
+	
+	db.Note.create(req.body)
+	  .then(function (dbNote) {
+		return db.Question.findOneAndUpdate({
+			_id: req.params.id
+		  }, {
+			note: dbNote._id
+		  }, {
+			new: true
+		  });
+		})
+		.then(function (dbQuestion) {
+			res.json(dbQuestion);
+		})
+		.catch(function (err) {
+		  res.json(err);
 		});
 	});
-});
-
-app.get("/note/:id", function(req, res) {
-	var id = req.params.id;
-	Article.findById(id).populate("note").exec(function(err, data) {
-		res.send(data.note);
-	})
-})
+	app.listen(PORT, function () {
+		console.log("App running on port " + PORT + "!");
+	  });
