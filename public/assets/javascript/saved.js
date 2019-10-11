141daf7d5bc3aee963c84cea8f3e2f0fd83c340c
Sam Tricamo <samualtricamo@gmail.com>
2019-10-10 23:39:14 -0500

    working on javascript

---
+++ public/assets/javascript/saved.js
@@ -0,0 +1,34 @@
+$(document.ready(function() {
+
+    var articleContainer = $(".article-container");
+
+    $(document).on("click", ".btn.delete", handleArticleDelete);
+    $(document).on("click", ".btn.notes", handleArticleDelete);
+    $(document).on("click", ".btn.save", handleArticleDelete);
+    $(document).on("click", ".btn.note-delete", handleArticleDelete);
+
+    initPage();
+
+    function initPage() {
+
+        articleContainer.empty();
+        $.get("/api/headlines?saved=true").then(function(data) {
+
+            if (data && data.length) {
+                renderArticles(data);
+            } else {
+                renderEmpty();
+            }
+        });
+    }
+
+
+function renderArticles(articles) {
+
+    var articlePanels = [];
+
+    for (var i = 0; i < articles.length; i++) {
+        articlePanels.push(createPanel(articles[i]));
+    }
+}
+}))
